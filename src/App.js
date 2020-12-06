7df0475f11eef9250a95808298f265a855aa7b85
AkashM398 <akashm1999@gmail.com>
2020-12-06 22:46:26 +0530

    Fixed Build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './css/App.css';
+import Header from "./components/header";
+import MainContent from "./components/viewContent1";
+import ViewContent2 from "./components/viewContent2"
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+          <Header />
+          <Switch>
+          <Route exact path="/" component={MainContent}/>
+          <Route path="/view1" component={MainContent}/>
+          <Route path="/view2" component={ViewContent2}/>
+          </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
